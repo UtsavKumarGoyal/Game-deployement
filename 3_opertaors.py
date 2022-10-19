c1e543cdc4411cc0dcc60a6289226aee5b86ec59
UtsavKumarGoyal <112815807+UtsavKumarGoyal@users.noreply.github.com>
2022-10-19 13:04:06 +0530

    Utsav

---
+++ 3_opertaors.py
@@ -0,0 +1,29 @@
+# arithmatic operators
+# a = 3
+from operator import truediv
+
+
+b = 4
+print("the value of 3+4 is", 3+4)
+print("the value of 3-4 is", 3-4)
+print("the value of 3*4 is", 3*4)
+print("the value of 3/4 is", 3/4)
+
+# assignment operators
+a = 34
+a *=10
+print(a)
+
+# comparison operators
+# a = (34<=36)
+# a = (34>=36)
+# a = (34==36)
+a = (34!=36)
+print(a)
+
+# logical operators
+bool1 = True
+bool2 = False
+print("the value of bool1 and bool2",bool1 and bool2)
+print("the value of bool1 or bool2",bool1 or bool2)
+print("the value of not bool2",not bool2)
